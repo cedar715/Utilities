fa66c1944e464c0e1d7c423373c6c94c443aeaaf
cedar715 <v.rao.jai@gmail.com>
2026-06-16 17:05:38 +0530

    Update CollectorConfig.java

--- CollectorConfig.java
+++ CollectorConfig.java
@@ -1,6 +1,8 @@
-# as appadmintemp, become solace (now works via the sudoers grant)
-sudo -u solace bash -c '
+appadmintemp@vm2[DEV][~] $ sudo -u solace bash -c '
   python3 -c "import podman_compose" 2>/dev/null && echo "already installed" && exit 0
   python3 -m pip install podman-compose --user --index-url <your py_artifactory value>
 '
 echo "exit=$?"
+[sudo] password for appadmintemp: 
+
+
