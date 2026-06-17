b0da65f43df000f1d0e96d3677e3c61a48d3901a
cedar715 <v.rao.jai@gmail.com>
2026-06-17 12:34:54 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,6 +1,23 @@
-grep -nE 'redundancy_group_node|nodetype|routername' /home/solace/solace/solace-compose.yml
-
-  # on VM3
-grep 'cncdcdev1monitor' /home/solace/solace/solace-compose.yml
-# on VM1
-grep 'cncdcdev1monitor' /home/solace/solace/solace-compose.yml
+[solace] | Host Boot ID: c497d9a8-3743-44bc-8840-3952e74f5e81
+[solace] | Starting PubSub+ Software Event Broker Container: Wed Jun 17 07:03:23 UTC 2026
+[solace] | Setting umask to 077
+[solace] | SolOS Version: soltr_10.25.0.217
+[solace] | ssh-keygen: generating new host keys: RSA DSA ECDSA ED25519 
+[solace] |   File: /
+[solace] |   Size: 50           Blocks: 0          IO Block: 4096   directory
+[solace] | Device: 31h/49d      Inode: 26433       Links: 1
+[solace] | Access: (0555/dr-xr-xr-x)  Uid: (    0/    root)   Gid: (    0/    root)
+[solace] | Access: 2026-06-17 07:03:34.554920689 +0000
+[solace] | Modify: 2026-06-17 07:03:22.920976478 +0000
+[solace] | Change: 2026-06-17 07:03:25.262965247 +0000
+[solace] |  Birth: 2026-06-17 07:03:22.920976478 +0000
+[solace] | unknownEarlyBirdLogPrefix: /opt/sbox/loadbuild/jenkins/slave/workspace/pubsubplus_git_10.25.0/solcbr/dataplane/sw/assuredDelivery/common/include/adCmnRuntime.hpp:104 WARN IOV_MAX is 1024
+[solace] | /usr/local/lib/python3.9/site-packages/flex/__init__.py:6: UserWarning: pkg_resources is deprecated as an API. See https://setuptools.pypa.io/en/latest/pkg_resources.html. The pkg_resources package is slated for removal as early as 2025-11-30. Refrain from using this package or pin to Setuptools<81.
+[solace] |   import pkg_resources
+[solace] | Unable to raise event; rc(would block)
+[solace] | consul: no process found
+[solace] | Snapshot session solace created.
+[solace] | Default snapshot output set to /usr/sw/lttng-traces/solace-20260617-070350
+[solace] | Every channel enabled for this session will be set to mmap output and default to overwrite mode.
+[solace] | ust event lttng_ust_tracef:* created in channel channel0
+[solace] | consul: no process found
