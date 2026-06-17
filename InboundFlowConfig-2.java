9df8be8ce0b809f05b7e9374d647af41ad97db13
cedar715 <v.rao.jai@gmail.com>
2026-06-17 09:57:08 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,4 +1,1 @@
-openssl rand -base64 33
-getent hosts cdc-dev1-a.51080.cn.app.example.com   # want 10.27.245.146 (VM1)
-getent hosts cdc-dev1-b.51080.cn.app.example.com   # want .147 (VM2)
-getent hosts cdc-dev1-c.51080.cn.app.example.com   # want .148 (VM3)
+JyHWzW46x/1ZluaFDYWqAXc/DI5G/XZn8CAJ3zuY7EQs
