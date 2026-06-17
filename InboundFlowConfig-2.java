39efb0a698063acbfe0ec6a44376c984b1bcb207
cedar715 <v.rao.jai@gmail.com>
2026-06-17 11:36:49 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,1 +1,2 @@
-podman exec solace sh -c 'ps aux | grep -i consul | grep -v grep'   # should show a consul process
+olace1> solace@cnlvatfss001[Dpodman exec solace sh -c 'ps aux | grep -i consul | grep -v grep'   # should show a consul processl process
+root         645  1.0  0.3 1350952 106044 ?      Sl   05:56   0:06 /usr/bin/consul agent -config-file /var/lib/solace/config/consul.json
