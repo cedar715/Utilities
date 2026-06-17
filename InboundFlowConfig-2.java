424855b94fe5f0eb4f5713e02dd220132444998e
cedar715 <v.rao.jai@gmail.com>
2026-06-17 07:52:13 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,5 +1,10 @@
 # what does id resolve to (the --user value podman-run passes)?
 id -u solace; id -g solace          # expect 5011 / 5011
-
+solace@myvm[~] $ id -u solace; id -g solace
+5011
+5011
+  
 # confirm the storage var path prod actually mounts
 grep -rn 'host_storage_group\|apps_root\|/var/lib/solace' roles/solace_runtime_deploy/ group_vars/ inventories/ | grep -iv '#' | head
+appadmintemp@myvm3[DEV][51080-solace-pubsubplus-podman-jgd] $ grep -rn 'host_storage_group\|apps_root\|/var/lib/solace' roles/solace_runtime_deploy/ group_vars/ inventories/ | grep -iv '#' | head
+roles/solace_runtime_deploy/tasks/main.yml:34:    - "{{ mounts.host_storage_group }}"
