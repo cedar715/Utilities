19ad5b48279e80bf0f9ced38cd578e1763c1e2ec
cedar715 <v.rao.jai@gmail.com>
2026-06-17 10:15:38 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,4 +1,19 @@
-solace@cnlvatfss003[DEV][~] $ podman unshare ls -lan /opt/apps/solace/storage-group  
-total 0
-drwxr-x---. 2 0 0  6 Jun 17 12:42 .
-drwxr-xr-x. 4 0 0 41 Jun 17 12:42 ..
+services:
+  solace:
+    image: artifactory.global.example.com/gv-images-products/oss/solace-pubsub-enterprise:10.25.0.217
+    container_name: solace
+    network_mode: host
+    uts: host
+    user: "0:0"
+    shm_size: 1g
+    ulimits:
+      core: -1
+      nofile:
+        soft: 2448
+        hard: 1048576
+    volumes:
+      - /opt/apps/solace/storage-group:/var/lib/solace:Z
+    environment:
+      username_admin_globalaccesslevel: admin
+      username_admin_password: <REAL-ADMIN-PASSWORD>
+      system_scaling_maxconnectioncount: "100"
