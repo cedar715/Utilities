6c3a57fd4e4f0444c2fb6d34efff50b2eb0d8c05
cedar715 <v.rao.jai@gmail.com>
2026-06-17 12:26:51 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,16 +1,12 @@
-# re-add the file-context equivalence rule (harmless if already present) and relabel RECURSIVELY
-sudo semanage fcontext -a -e /var/lib/containers /opt/apps/solace/podman/graphroot 2>/dev/null
-sudo restorecon -R -v /opt/apps/solace/podman/graphroot
-
-# verify it's now container_var_lib_t
-sudo ls -laZd /opt/apps/solace/podman/graphroot
-
-
-  # back as solace on VM3
-systemctl --user start solace.service
-sleep 20
-podman ps --format '{{.Names}} {{.Status}}'           # solace = Up now
-podman logs solace 2>&1 | grep -iE 'platform type|pre-startup'   # [ OK ] lines
-
-
-  sudo semanage fcontext -l | grep '/opt/apps/solace/podman/graphroot'
+[solace] | Host Boot ID: c497d9a8-3743-44bc-8840-3952e74f5e81
+[solace] | Starting PubSub+ Software Event Broker Container: Wed Jun 17 06:56:20 UTC 2026
+[solace] | Setting umask to 077
+[solace] | SolOS Version: soltr_10.25.0.217
+[solace] | Failed to render config files, exiting (rc = 1)
+[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Backend set to env
+[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Starting confd
+[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Backend source(s) set to 
+[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: ERROR template: dbBaseline.tmpl:245:25: executing "dbBaseline.tmpl" at <getv $key_nodetype>: error calling getv: key does not exist: /redundancy/group/node/cncdcdev1monitor/nodetype
+[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Target config /etc/ssh/sshd_config out of sync
+[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Target config /etc/ssh/sshd_config has been updated
+[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: FATAL template: dbBaseline.tmpl:245:25: executing "dbBaseline.tmpl" at <getv $key_nodetype>: error calling getv: key does not exist: /redundancy/group/node/cncdcdev1monitor/nodetype
