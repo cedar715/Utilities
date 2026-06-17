6efd5ed2c350fbbf08ebe53eb2a0fa07634b5443
cedar715 <v.rao.jai@gmail.com>
2026-06-17 09:19:03 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,7 +1,17 @@
-# as appadmintemp
-stat -fc %T /sys/fs/cgroup                                    # cgroup2fs
-loginctl show-user solace -p Linger                          # Linger=yes (may need enabling)
-systemctl show user@5011.service -p LimitNOFILE              # 1048576 (may need the drop-in)
-grep solace /etc/subuid                                       # range exists
-ls -laZd /opt/apps/solace/podman/graphroot 2>/dev/null       # container_var_lib_t (if relocated)
-ss -tlnp | grep ':2222'                                       # does host sshd own 2222 here too?
+appadmintemp@cnlvatfss003[DEV][~] $ stat -fc %T /sys/fs/cgroup   
+cgroup2fs
+appadmintemp@cnlvatfss003[DEV][~] $ loginctl show-user solace -p Linger   
+Linger=yes
+appadmintemp@cnlvatfss003[DEV][~] $ systemctl show user@5011.service -p LimitNOFILE     
+LimitNOFILE=524288
+appadmintemp@cnlvatfss003[DEV][~] $ grep solace /etc/subuid         
+solace:689824:65536
+solacex:820896:65536
+appadmintemp@cnlvatfss003[DEV][~] $ ls -laZd /opt/apps/solace/podman/graphroot 2>/dev/null  
+appadmintemp@cnlvatfss003[DEV][~] $ ls -laZd /opt/apps/solace/podman/graphroot
+ls: cannot access '/opt/apps/solace/podman/graphroot': Permission denied
+appadmintemp@cnlvatfss003[DEV][~] $ sudo ls -laZd /opt/apps/solace/podman/graphroot
+drwxr-x---. 10 solace solace unconfined_u:object_r:unlabeled_t:s0 4096 Jun 17 11:45 /opt/apps/solace/podman/graphroot
+appadmintemp@cnlvatfss003[DEV][~] $ ss -tlnp | grep ':2222'   
+appadmintemp@cnlvatfss003[DEV][~] $ 
+
