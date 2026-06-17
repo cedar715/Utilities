4e24878f84dc836bf18a2b3d1648e7ca6e58cdaa
cedar715 <v.rao.jai@gmail.com>
2026-06-17 10:46:19 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,7 +1,5 @@
-# make secrets owned by container-0 (= host solace 5011), matching VM1 and the user:"0:0" model
-podman unshare chown -R 0:0 /home/solace/secrets
-podman unshare ls -lan /home/solace/secrets   # now owner 0 0
-# then the direct write works (you're solace, dir is solace-owned now)
-echo -n '<REAL-ADMIN-PASSWORD>' > /home/solace/secrets/username_admin_password
-echo -n 'JyHWzW46x/1ZluaFDYWqAXc/DI5G/XZn8CAJ3zuY7EQs' > /home/solace/secrets/solace-preshared-key.conf
-chmod 600 /home/solace/secrets/*
+solace@cnlvatfss003[DEV][~] $ podman unshare chown -R 0:0 /home/solace/secrets
+chown: changing ownership of '/home/solace/secrets/username_admin_password': Operation not permitted
+chown: changing ownership of '/home/solace/secrets/server.cert': Operation not permitted
+chown: changing ownership of '/home/solace/secrets': Operation not permitted
+solace@cnlvatfss003[DEV][~] $ 
