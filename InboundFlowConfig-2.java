abc34ccae44877264c3c1ce166963913f9000846
cedar715 <v.rao.jai@gmail.com>
2026-06-17 12:20:26 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -4,3 +4,13 @@ sudo restorecon -R -v /opt/apps/solace/podman/graphroot
 
 # verify it's now container_var_lib_t
 sudo ls -laZd /opt/apps/solace/podman/graphroot
+
+
+  # back as solace on VM3
+systemctl --user start solace.service
+sleep 20
+podman ps --format '{{.Names}} {{.Status}}'           # solace = Up now
+podman logs solace 2>&1 | grep -iE 'platform type|pre-startup'   # [ OK ] lines
+
+
+  sudo semanage fcontext -l | grep '/opt/apps/solace/podman/graphroot'
