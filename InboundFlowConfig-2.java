39af526f4eec6f0c52b1839d539030eef7ea0711
cedar715 <v.rao.jai@gmail.com>
2026-06-17 11:17:54 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,1 +1,9 @@
 mkdir -p /home/solace/solace
+ls -ld /home/solace/solace    # owner 760370 or similar = the problem
+
+  sudo chown 5011:5011 /home/solace/solace
+
+  mkdir -p /home/solace/solace
+# create the file
+vi /home/solace/solace/solace-compose.yml
+# (:set paste first to avoid indent corruption, then paste)
