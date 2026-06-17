9c126a00a5da7c88efc8e847b02961d4f6163216
cedar715 <v.rao.jai@gmail.com>
2026-06-17 12:13:51 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,3 +1,8 @@
-cd /home/solace/solace
-/home/solace/.local/bin/podman-compose -f solace-compose.yml up
-# let it print the error, then Ctrl-C
+solace@cnlvatfss003[DEV][solace] $ /home/solace/.local/bin/podman-compose -f solace-compose.yml up
+c74626cef70f058d792497af012c27a4d9a6e06a950d61d8a3110430fdd0e4cb
+323253acab6b03de1f7d0e36d39095eb2f326abd89606b4271e99f49ad1c5b33
+Port mappings have been discarded as one of the Host, Container, Pod, and None network modes are in use
+c6ab34d9b0939d3db1b47b9bbee321a2d22f13a5642f14521e5457bf0d9ae45c
+[solace] | ERROR: ld.so: object '/usr/sw/loads/currentload/lib64/altsyslog.so' from /etc/ld.so.preload cannot be preloaded (cannot change memory protections): ignored.
+[solace] | /bin/bash: error while loading shared libraries: /lib64/libc.so.6: cannot apply additional memory protection after relocation: Permission denied
+solace@cnlvatfss003[DEV][solace] $ 
