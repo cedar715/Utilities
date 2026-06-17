6d7f9bedb693ce93c4ae81b5c849f58c0a09bf02
cedar715 <v.rao.jai@gmail.com>
2026-06-17 12:28:50 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,12 +1,6 @@
-[solace] | Host Boot ID: c497d9a8-3743-44bc-8840-3952e74f5e81
-[solace] | Starting PubSub+ Software Event Broker Container: Wed Jun 17 06:56:20 UTC 2026
-[solace] | Setting umask to 077
-[solace] | SolOS Version: soltr_10.25.0.217
-[solace] | Failed to render config files, exiting (rc = 1)
-[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Backend set to env
-[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Starting confd
-[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Backend source(s) set to 
-[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: ERROR template: dbBaseline.tmpl:245:25: executing "dbBaseline.tmpl" at <getv $key_nodetype>: error calling getv: key does not exist: /redundancy/group/node/cncdcdev1monitor/nodetype
-[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Target config /etc/ssh/sshd_config out of sync
-[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: INFO Target config /etc/ssh/sshd_config has been updated
-[solace] | 2026-06-17T06:56:22Z solace2 /usr/sbin/confd[95]: FATAL template: dbBaseline.tmpl:245:25: executing "dbBaseline.tmpl" at <getv $key_nodetype>: error calling getv: key does not exist: /redundancy/group/node/cncdcdev1monitor/nodetype
+grep -nE 'redundancy_group_node|nodetype|routername' /home/solace/solace/solace-compose.yml
+
+  # on VM3
+grep 'cncdcdev1monitor' /home/solace/solace/solace-compose.yml
+# on VM1
+grep 'cncdcdev1monitor' /home/solace/solace/solace-compose.yml
