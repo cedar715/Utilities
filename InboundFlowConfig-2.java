226510211dc01d5dc9589fa2f69f23eb4e32d982
cedar715 <v.rao.jai@gmail.com>
2026-06-17 12:12:38 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,6 +1,8 @@
-# on VM3 (monitor)
-systemctl --user start solace.service
-systemctl --user status solace.service --no-pager | grep Active:
-podman ps --format '{{.Names}} {{.Status}}'
-podman logs solace 2>&1 | tail -20
-  
+solace@cnlvatfss003[DEV][~] $ systemctl --user start solace.service
+solace@cnlvatfss003[DEV][~] $ systemctl --user status solace.service --no-pager | grep Active:
+     Active: inactive (dead) since Wed 2026-06-17 14:41:59 CST; 7s ago
+solace@cnlvatfss003[DEV][~] $ podman ps --format '{{.Names}} {{.Status}}'
+solace@cnlvatfss003[DEV][~] $ podman logs solace 2>&1 | tail -20
+Error: no container with name or ID "solace" found: no such container
+solace@cnlvatfss003[DEV][~] $ 
+
