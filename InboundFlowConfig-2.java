11f277c700e056e306af05622d1b752bde5fe9ef
cedar715 <v.rao.jai@gmail.com>
2026-06-17 12:13:13 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,8 +1,3 @@
-solace@cnlvatfss003[DEV][~] $ systemctl --user start solace.service
-solace@cnlvatfss003[DEV][~] $ systemctl --user status solace.service --no-pager | grep Active:
-     Active: inactive (dead) since Wed 2026-06-17 14:41:59 CST; 7s ago
-solace@cnlvatfss003[DEV][~] $ podman ps --format '{{.Names}} {{.Status}}'
-solace@cnlvatfss003[DEV][~] $ podman logs solace 2>&1 | tail -20
-Error: no container with name or ID "solace" found: no such container
-solace@cnlvatfss003[DEV][~] $ 
-
+cd /home/solace/solace
+/home/solace/.local/bin/podman-compose -f solace-compose.yml up
+# let it print the error, then Ctrl-C
