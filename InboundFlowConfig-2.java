5f78ceef87bc583caffe5e7a253c0d882168df21
cedar715 <v.rao.jai@gmail.com>
2026-06-17 10:40:53 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,11 +1,2 @@
-ls -lan /home/solace/secrets
-ls -ld /home/solace/secrets
-
-  # move the old one aside, make a fresh solace-owned dir
-podman unshare rm -rf /home/solace/secrets 2>/dev/null
-mkdir -p /home/solace/secrets
-ls -ld /home/solace/secrets    # should now be solace:solace
-echo -n '<REAL-ADMIN-PASSWORD>' > /home/solace/secrets/username_admin_password
-echo -n 'JyHWzW46x/1ZluaFDYWqAXc/DI5G/XZn8CAJ3zuY7EQs' > /home/solace/secrets/solace-preshared-key.conf
-chmod 600 /home/solace/secrets/*
-ls -lan /home/solace/secrets    # confirm files exist, owned by solace (5011)
+solace@cnlvatfss003[DEV][~] $ ls -ld /home/solace/secrets    # should now be solace:solace
+drwxr-x---. 2 760370 solace 89 Jun 16 02:34 /home/solace/secrets
