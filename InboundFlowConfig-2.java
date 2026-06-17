36f2a581e992395715ba005886cff368a8c864eb
cedar715 <v.rao.jai@gmail.com>
2026-06-17 12:39:29 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,2 +1,7 @@
 podman logs solace 2>&1 | tail -40
 journalctl --user -u solace.service --no-pager -n 30
+
+
+  grep -nE 'activestandbyrole|matelink|configsync|nodetype|routername|redundancy' /home/solace/solace/solace-compose.yml
+  # on each node
+md5sum /home/solace/secrets/solace-preshared-key.conf 2>/dev/null || podman unshare md5sum /home/solace/secrets/solace-preshared-key.conf
