33cb133a576b7612b9d55e1f0a89b8c67e7d4006
cedar715 <v.rao.jai@gmail.com>
2026-06-17 09:27:38 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,2 +1,6 @@
-sudo cat /etc/systemd/system/user@5011.service.d/delegate.conf 2>/dev/null || echo "MISSING"
-sudo ls /etc/systemd/system/user@5011.service.d/
+appadmintemp@cnlvatfss003[DEV][51080-solace-pubsubplus-podman-jgd] $ sudo cat /etc/systemd/system/user@5011.service.d/delegate.conf 2>/dev/null || echo "MISSING"
+[Service]
+Delegate=memory pids cpu cpuset
+appadmintemp@cnlvatfss003[DEV][51080-solace-pubsubplus-podman-jgd] $ sudo ls /etc/systemd/system/user@5011.service.d/
+delegate.conf
+appadmintemp@cnlvatfss003[DEV][51080-solace-pubsubplus-podman-jgd] $ 
