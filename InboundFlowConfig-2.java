4981d6b2cc99f077432b3a39b856de71d8b92ad4
cedar715 <v.rao.jai@gmail.com>
2026-06-17 11:27:09 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -7,3 +7,8 @@ systemctl --user start solace.service
 # wait ~60s for primary+backup to find each other and establish the mate link
 # THEN VM3 (monitor)
 systemctl --user start solace.service
+
+  ---
+  systemctl --user status solace.service --no-pager | grep -E 'Active:|Main PID:'
+podman ps --format '{{.Names}} {{.Status}}'          # solace = Up, not Created/restarting
+podman logs solace 2>&1 | grep -iE 'platform type|pre-startup'   # [ OK ] lines
