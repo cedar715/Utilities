6b2420a86b932fed84f5a908b787a7c187b94db9
cedar715 <v.rao.jai@gmail.com>
2026-06-17 09:26:57 +0530

    Update InboundFlowConfig-2.java

--- InboundFlowConfig-2.java
+++ InboundFlowConfig-2.java
@@ -1,2 +1,2 @@
-sudo ls -laZd /opt/apps/solace/podman/graphroot
-drwxr-x---. 10 solace solace unconfined_u:object_r:container_var_lib_t:s0 4096 Jun 17 11:45 /opt/apps/solace/podman/graphroot
+sudo cat /etc/systemd/system/user@5011.service.d/delegate.conf 2>/dev/null || echo "MISSING"
+sudo ls /etc/systemd/system/user@5011.service.d/
