bc4923f2cdb5d4c1449f29c856ccc5061c776aff
cedar715 <v.rao.jai@gmail.com>
2026-07-28 22:52:14 +0530

    Update KafkaConfig.java

--- KafkaConfig.java
+++ KafkaConfig.java
@@ -1,118 +1,83 @@
-# SYSTEM_HA_REDUN_GROUP_NODE_LEFT - v2
-#
-# Replaces the increase()/unless approach, which fired based on when old events
-# aged out of a 24h window rather than when events actually occurred.
-#
-# How this works:
-#   left_total - joined_total  = number of nodes currently out of the redundancy group,
-#                                plus a fixed per-host offset from unmatched startup joins.
-#   The median over 3 days     = that fixed offset (the "everything healthy" baseline).
-#   Subtracting the two        = 0 when healthy, >0 during an actual outage.
-#
-# Median (quantile 0.5) is used rather than min_over_time because the difference
-# excursions in both directions; min would latch onto a downward transient and
-# hold a false baseline for 3 days.
-#
-# BEFORE DEPLOYING: route this to a test contact point and shadow-run for a week
-# against Kibana before pointing it at the production receiver.
+        - uid: ef3wvtjhxbh1cd
+          title: VPN_AD_MSG_SPOOL_QUOTA_EXCEED
+          condition: B
+          data:
+            - refId: A
+              relativeTimeRange:
+                from: 1800
+                to: 0
+              datasourceUid: c2d2d69e-138c-462b-9f5b-84e530148fae
+              model:
+                datasource:
+                    type: prometheus
+                    uid: c2d2d69e-138c-462b-9f5b-84e530148fae
+                editorMode: code
+                expr: "clamp_min(\r\n  (\r\n    sum by (host, queue, vmrHostAlias, vpn) (increase(sol_event_vpn_ad_msg_spool_quota_exceed_total[1d]))\r\n    unless\r\n    sum by (host, queue, vmrHostAlias, vpn) (increase(sol_event_vpn_ad_msg_spool_high_clear_total[1d])) > 0\r\n  ),\r\n  0\r\n)\r\n"
+                instant: true
+                intervalMs: 1000
+                legendFormat: __auto
+                maxDataPoints: 43200
+                range: false
+                refId: A
+            - refId: B
+              relativeTimeRange:
+                from: 600
+                to: 0
+              datasourceUid: __expr__
+              model:
+                conditions:
+                    - evaluator:
+                        params:
+                            - 0
+                            - 0
+                        type: gt
+                      operator:
+                        type: and
+                      query:
+                        params: []
+                      reducer:
+                        params: []
+                        type: avg
+                      type: query
+                datasource:
+                    name: Expression
+                    type: __expr__
+                    uid: __expr__
+                expression: $A > 0
+                intervalMs: 1000
+                maxDataPoints: 43200
+                refId: B
+                type: math
+          dashboardUid: aegbeyarmdc00a
+          panelId: 1
+          noDataState: OK
+          execErrState: KeepLast
+          annotations:
+            __dashboardUid__: aegbeyarmdc00a
+            __panelId__: "1"
+            description: |-
+                Documentation: https://docs.solace.com/Admin-Ref/Solace-PubSub-Event-Reference/event_ref_boiler.html#VPN_AD_MSG_SPOOL_QUOTA_EXCEED
 
-- uid: aexqk11rdophca-v2
-  title: SYSTEM_HA_REDUN_GROUP_NODE_LEFT STS
-  condition: B
-  # Pending period: condition must hold 5 continuous minutes before firing.
-  # Observed real outages last 20-30 min, so this is safely inside them.
-  for: 5m
-  data:
-    - refId: A
-      relativeTimeRange:
-        from: 600
-        to: 0
-      datasourceUid: c2d2d69e-138c-462b-9f5b-84e530148fae
-      model:
-        datasource:
-          type: prometheus
-          uid: c2d2d69e-138c-462b-9f5b-84e530148fae
-        editorMode: code
-        expr: |-
-          (
-            sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
-            -
-            sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
-          )
-          -
-          quantile_over_time(0.5,
-            (
-              sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
-              -
-              sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
-            )[3d:5m]
-          )
-        instant: true
-        intervalMs: 1000
-        legendFormat: __auto
-        maxDataPoints: 43200
-        range: false
-        refId: A
-
-    - refId: B
-      relativeTimeRange:
-        from: 600
-        to: 0
-      datasourceUid: __expr__
-      model:
-        datasource:
-          name: Expression
-          type: __expr__
-          uid: __expr__
-        expression: $A > 0
-        intervalMs: 1000
-        maxDataPoints: 43200
-        refId: B
-        type: math
-
-  dashboardUid: arHPdgYmk
-  panelId: 112
-
-  noDataState: OK
-  # Error (not KeepLast) so Prometheus query failures are visible instead of
-  # silently freezing the alert in its previous state.
-  execErrState: Error
-
-  annotations:
-    __dashboardUid__: arHPdgYmk
-    __panelId__: "112"
-    description: >-
-      HA node left the redundancy group on {{ $labels.host }}
-      ({{ $labels.vmrHostAlias }}). Nodes currently out: {{ $values.A.Value }}.
-    summary: "Documentation: https://docs.solace.com/Solace-PubSub-Event-Reference/event_ref_boiler.html#SYSTEM_HA_REDUN_GROUP_NODE_LEFT \n\nKibana: https://kibana.example.com/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:120000),time:(from:now-1d,to:now))&_a=(columns:!(vmrHost,vmrHostAlias,syslogEvent,message),dataSource:(dataViewId:ef11adb1-12c0-4374-805c-1d8a8fd4dde1,type:dataView),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,field:appId,index:'logs-*',key:appId,negate:!f,params:(query:'myID'),type:phrase),query:(match_phrase:(appId:'myID'))),('$state':(store:appState),meta:(alias:!n,disabled:!f,field:syslogEvent,index:ef11adb1-12c0-4374-805c-1d8a8fd4dde1,key:syslogEvent,negate:!f,params:!(SYSTEM_HA_REDUN_GROUP_NODE_LEFT,SYSTEM_HA_REDUN_GROUP_NODE_JOINED),type:phrases,value:!(SYSTEM_HA_REDUN_GROUP_NODE_LEFT,SYSTEM_HA_REDUN_GROUP_NODE_JOINED)),query:(bool:(minimum_should_match:1,should:!((match_phrase:(syslogEvent:SYSTEM_HA_REDUN_GROUP_NODE_LEFT)),(match_phrase:(syslogEvent:SYSTEM_HA_REDUN_GROUP_NODE_JOINED))))))),grid:(columns:(solaceEventData.client:(width:436),syslogEvent:(width:275),vmrHost:(width:190),vmrHostAlias:(width:211))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))"
-
-  labels:
-    CI: "myID"
-    SupportGroup: ""
-    alertmetric: Application
-    alertparam: Usage
-    alertresource: Application
-    alerttype: Pod
-    app_id: "myID"
-    app_name: FOUNDATION SERVICES SOLACE BA
-    auto_inc: ""
-    cmsid: COP-GRAF-001
-    itam-id: "myID"
-    itam_id: "myID"
-    name: SYSTEM_HA_REDUN_GROUP_NODE_LEFT STS
-    severity: error
-    source: grafana
-    supportgroup: ""
-    value: ""
-
-  isPaused: false
-
-  notification_settings:
-    # CHANGE THIS to a test receiver for the shadow-run period.
-    receiver: "myID"
-    group_by:
-      - grafana_folder
-      - alertname
-      - host
-      - vmrHostAlias
-    repeat_interval: 30m
+                Kibana: https://kibana.example.com/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:now-1w,to:now))&_a=(columns:!(solaceEventData.queue,syslogEvent,vmrHostAlias,solaceEventData.vpn,message),dataSource:(dataViewId:'ef11adb1-12c0-4374-805c-1d8a8fd4dde1',type:dataView),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'ef11adb1-12c0-4374-805c-1d8a8fd4dde1',key:syslogEvent,negate:!f,params:!(VPN_AD_MSG_SPOOL_QUOTA_EXCEED,VPN_AD_MSG_SPOOL_HIGH_CLEAR),type:phrases,value:!(VPN_AD_MSG_SPOOL_QUOTA_EXCEED,VPN_AD_MSG_SPOOL_HIGH_CLEAR)),query:(bool:(minimum_should_match:1,should:!((match_phrase:(syslogEvent:VPN_AD_MSG_SPOOL_QUOTA_EXCEED)),(match_phrase:(syslogEvent:VPN_AD_MSG_SPOOL_HIGH_CLEAR))))))),grid:(columns:(solaceEventData.queue:(width:284),solaceEventData.vpn:(width:170),syslogEvent:(width:260),vmrHostAlias:(width:154))),interval:auto,query:(language:kuery,query:'solaceEventData.vpn%20:%20%22{{ $labels.vpn }}%22%20and%20vmrHostAlias%20:%20%22{{ $labels.vmrHostAlias }}%22%20and%20solaceEventData.queue%20:%20%22{{ $labels.queue }}%22'),sort:!(!('@timestamp',desc)))
+            summary: "Warning: The total size of messages in the VPN: {{ $labels.vpn }} / Queue: {{ $labels.queue }}  has exceeded its configured quota. Messages may be discarded until usage drops below the limit. Further, \nthe spooled messages if not consumed with-in 48hrs, would be purged.\n\nGrafana Dashboard:\nhttps://grafana.cop.awscloud.sc.net/d/solacequeuedb4clients/solace-clients-queue-dashboard?orgId=1&var-vpn={{ $labels.vpn }}&var-queue={{ $labels.queue }}"
+          labels:
+            CI: "myID"
+            SupportGroup: ""
+            alertmetric: Network Connectivity
+            alertparam: Usage
+            alertresource: Network Connectivity
+            alerttype: Connectivity
+            app_id: "myID"
+            app_name: FOUNDATION SERVICES SOLACE BA
+            auto_inc: ""
+            client-itam-field: '{{ reReplaceAll `^q-([0-9]+)-.*` "$${1}" $$labels.queue }}'
+            client-notification: "true"
+            cmsid: COP-GRAF-001
+            itam-id: "myID"
+            itam_id: "myID"
+            name: VPN_AD_MSG_SPOOL_QUOTA_EXCEED
+            severity: critical
+            source: grafana
+            supportgroup: ""
+            value: ""
+          isPaused: true
