cd7e83ebe87929c90e4c6b81270517290ea11318
cedar715 <v.rao.jai@gmail.com>
2026-07-28 21:11:32 +0530

    Update KafkaConfig.java

--- KafkaConfig.java
+++ KafkaConfig.java
@@ -1,3 +1,13 @@
-sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
+(
+  sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
+  -
+  sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
+)
 -
-sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
+quantile_over_time(0.5,
+  (
+    sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
+    -
+    sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
+  )[3d:5m]
+)
