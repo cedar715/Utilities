cfd3bda6b8631bb4804c4a6c6338ba56e4fb2469
cedar715 <v.rao.jai@gmail.com>
2026-07-28 20:53:08 +0530

    Update KafkaConfig.java

--- KafkaConfig.java
+++ KafkaConfig.java
@@ -12,3 +12,12 @@ count by (host, queue, vmrHostAlias, vpn) (sol_event_system_ha_redun_group_node_
 
 # 4. Scrape gaps (staleness can also fake a reset)
 up{job=~".*solace.*"}
+---
+sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
+-
+sum by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
+
+  sol_event_system_ha_redun_group_node_left_total
+                             sol_event_system_ha_redun_group_node_joined_total
+
+  resets(sol_event_system_ha_redun_group_node_left_total[7d])
