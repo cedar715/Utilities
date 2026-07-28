71267b1963c62ffac4dd1dd3edd927964e08c9ae
cedar715 <v.rao.jai@gmail.com>
2026-07-29 00:28:45 +0530

    Update KafkaConfig.java

--- KafkaConfig.java
+++ KafkaConfig.java
@@ -1,37 +1,3 @@
-max by (host, vmrHostAlias, vpn, queue) (
-  max_over_time(
-    (timestamp(
-      ((sol_event_vpn_ad_msg_spool_quota_exceed_total
-        - sol_event_vpn_ad_msg_spool_quota_exceed_total offset 10m) > 0)
-      or
-      (sol_event_vpn_ad_msg_spool_quota_exceed_total
-       unless (sol_event_vpn_ad_msg_spool_quota_exceed_total offset 10m))
-    ))[12h:1m]
-  )
-)
-or
-(
-  max by (host, vmrHostAlias, vpn, queue) (sol_event_vpn_ad_msg_spool_quota_exceed_total)
-  or
-  max by (host, vmrHostAlias, vpn, queue) (sol_event_vpn_ad_msg_spool_high_clear_total)
-) * 0
+sol_event_vpn_client_username_connections_exceeded_total
 
-  ---
-
-  max by (host, vmrHostAlias, vpn, queue) (
-  max_over_time(
-    (timestamp(
-      ((sol_event_vpn_ad_msg_spool_high_clear_total
-        - sol_event_vpn_ad_msg_spool_high_clear_total offset 10m) > 0)
-      or
-      (sol_event_vpn_ad_msg_spool_high_clear_total
-       unless (sol_event_vpn_ad_msg_spool_high_clear_total offset 10m))
-    ))[12h:1m]
-  )
-)
-or
-(
-  max by (host, vmrHostAlias, vpn, queue) (sol_event_vpn_ad_msg_spool_quota_exceed_total)
-  or
-  max by (host, vmrHostAlias, vpn, queue) (sol_event_vpn_ad_msg_spool_high_clear_total)
-) * 0
+  sol_event_vpn_client_username_connections_high_clear_total
