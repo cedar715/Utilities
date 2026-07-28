dc24a8902c1f18f59546853c4d796273273e6c81
cedar715 <v.rao.jai@gmail.com>
2026-07-29 00:07:24 +0530

    Update KafkaConfig.java

--- KafkaConfig.java
+++ KafkaConfig.java
@@ -1,2 +1,35 @@
-count(sol_event_system_ha_redun_group_node_left_total)
-count(sol_event_system_ha_redun_group_node_joined_total)
+max by (host, vmrHostAlias) (
+  max_over_time(
+    (timestamp(
+      ((sol_event_system_ha_redun_group_node_left_total
+        - sol_event_system_ha_redun_group_node_left_total offset 10m) > 0)
+      or
+      (sol_event_system_ha_redun_group_node_left_total
+       unless (sol_event_system_ha_redun_group_node_left_total offset 10m))
+    ))[12h:1m]
+  )
+)
+or
+(
+  max by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
+  or
+  max by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
+) * 0
+  ---
+  max by (host, vmrHostAlias) (
+  max_over_time(
+    (timestamp(
+      ((sol_event_system_ha_redun_group_node_joined_total
+        - sol_event_system_ha_redun_group_node_joined_total offset 10m) > 0)
+      or
+      (sol_event_system_ha_redun_group_node_joined_total
+       unless (sol_event_system_ha_redun_group_node_joined_total offset 10m))
+    ))[12h:1m]
+  )
+)
+or
+(
+  max by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
+  or
+  max by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
+) * 0
