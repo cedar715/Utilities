bce2e956d04bfa64b374e3c6e57e93cf811c5b8b
cedar715 <v.rao.jai@gmail.com>
2026-07-29 00:14:19 +0530

    Update KafkaConfig.java

--- KafkaConfig.java
+++ KafkaConfig.java
@@ -1,1 +1,37 @@
-HA node left the redundancy group on {{ $labels.host }} ({{ $labels.vmrHostAlias }}) and has not rejoined.
+max by (host, vmrHostAlias, vpn, queue) (
+  max_over_time(
+    (timestamp(
+      ((sol_event_vpn_ad_msg_spool_quota_exceed_total
+        - sol_event_vpn_ad_msg_spool_quota_exceed_total offset 10m) > 0)
+      or
+      (sol_event_vpn_ad_msg_spool_quota_exceed_total
+       unless (sol_event_vpn_ad_msg_spool_quota_exceed_total offset 10m))
+    ))[12h:1m]
+  )
+)
+or
+(
+  max by (host, vmrHostAlias, vpn, queue) (sol_event_vpn_ad_msg_spool_quota_exceed_total)
+  or
+  max by (host, vmrHostAlias, vpn, queue) (sol_event_vpn_ad_msg_spool_high_clear_total)
+) * 0
+
+  ---
+
+  max by (host, vmrHostAlias, vpn, queue) (
+  max_over_time(
+    (timestamp(
+      ((sol_event_vpn_ad_msg_spool_high_clear_total
+        - sol_event_vpn_ad_msg_spool_high_clear_total offset 10m) > 0)
+      or
+      (sol_event_vpn_ad_msg_spool_high_clear_total
+       unless (sol_event_vpn_ad_msg_spool_high_clear_total offset 10m))
+    ))[12h:1m]
+  )
+)
+or
+(
+  max by (host, vmrHostAlias, vpn, queue) (sol_event_vpn_ad_msg_spool_quota_exceed_total)
+  or
+  max by (host, vmrHostAlias, vpn, queue) (sol_event_vpn_ad_msg_spool_high_clear_total)
+) * 0
