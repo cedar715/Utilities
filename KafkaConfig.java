d12b46d56ec73a54befc6eb6af38dbcfd252921f
cedar715 <v.rao.jai@gmail.com>
2026-07-28 23:41:18 +0530

    Update KafkaConfig.java

--- KafkaConfig.java
+++ KafkaConfig.java
@@ -9,17 +9,9 @@ max by (host, vmrHostAlias) (
     ))[1d:5m]
   )
 )
-
-  max by (host, vmrHostAlias) (
-  max_over_time(
-    (timestamp(
-      ((sol_event_system_ha_redun_group_node_joined_total
-        - sol_event_system_ha_redun_group_node_joined_total offset 10m) > 0)
-      or
-      (sol_event_system_ha_redun_group_node_joined_total
-       unless (sol_event_system_ha_redun_group_node_joined_total offset 10m))
-    ))[1d:5m]
-  )
-)
 or
-max by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total) * 0
+(
+  max by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_left_total)
+  or
+  max by (host, vmrHostAlias) (sol_event_system_ha_redun_group_node_joined_total)
+) * 0
